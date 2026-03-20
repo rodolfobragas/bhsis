e71aa70dfeff7ff07a87d7df049e8846bb0c4b1f
Rodolfo <rodolfobragas@gmail.com>
2026-03-20 19:06:31 -0300

    fix: proxy /api no dev server

--- bhsis/vite.config.ts
+++ bhsis/vite.config.ts
@@ -183,5 +183,11 @@ export default defineConfig({
       strict: true,
       deny: ["**/.*"],
     },
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
   },
 });
